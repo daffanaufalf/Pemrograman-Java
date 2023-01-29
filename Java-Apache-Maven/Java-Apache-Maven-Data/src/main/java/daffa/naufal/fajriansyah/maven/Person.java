de097b4b8a18d637938f57cc951feeca45fdfde6
wellplayed <daffanfproject@gmail.com>
2023-01-29 13:34:07 +0700

    Apache Maven

---
+++ Java-Apache-Maven/Java-Apache-Maven-Data/src/main/java/daffa/naufal/fajriansyah/maven/Person.java
@@ -0,0 +1,20 @@
+package daffa.naufal.fajriansyah.maven;
+
+public class Person {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+
+}
