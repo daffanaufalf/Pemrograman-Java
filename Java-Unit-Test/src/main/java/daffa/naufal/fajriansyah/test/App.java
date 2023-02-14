f2bdf593b8aade2a153c91a64a0bee7c0e0a6949
wellplayed <daffanfproject@gmail.com>
2023-02-14 17:28:17 +0700

    first commit

---
+++ Java-Unit-Test/src/main/java/daffa/naufal/fajriansyah/test/App.java
@@ -0,0 +1,13 @@
+package daffa.naufal.fajriansyah.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
